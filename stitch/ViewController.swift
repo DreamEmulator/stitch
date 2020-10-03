d658f74e934caa9b208d6395f3af3496484fdd1d
Sebastiaan Hols <s.hols@icloud.com>
2020-10-03 09:21:09 +0200

    Initial Commit

---
+++ stitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stitch
+//
+//  Created by Sebastiaan Hols on 03/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
