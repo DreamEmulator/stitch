639df2311cc3a0d0492c15baa80e59b701b1cba0
Sebastiaan Hols <s.hols@icloud.com>
2020-10-03 11:17:03 +0200

    Select play and pause music

--- stitch/ViewController.swift
+++ stitch/ViewController.swift
@@ -10,6 +10,12 @@ import MediaPlayer
 
 class ViewController: UIViewController, MPMediaPickerControllerDelegate {
 
+    let musicPlayer = MPMusicPlayerController.systemMusicPlayer
+    var track: MPMediaItem?;
+    
+    @IBOutlet weak var trackInfo: UIView!
+    @IBOutlet weak var trackName: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
     
     @IBAction func pickTrack(_ sender: UIButton) {
         let controller = MPMediaPickerController(mediaTypes: .music)
@@ -17,6 +23,35 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         controller.popoverPresentationController?.sourceView = sender
         controller.delegate = self
         present(controller, animated: true)
+    }
+    
+    @IBAction func playTrackPressed(_ sender: UIButton) {
+        if(track != nil) {
+            playTrack()
+        }
+    }
+    
+    func playTrack (){
+        if (musicPlayer.playbackState != .playing){
+            musicPlayer.play();
+            playPauseButton.setImage(UIImage(systemName:"pause.fill"), for: .normal)
+        } else {
+            musicPlayer.pause();
+            playPauseButton.setImage(UIImage(systemName:"play.fill"), for: .normal)
+        }
+    }
+    
+    func mediaPicker(_ mediaPicker: MPMediaPickerController,
+                     didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        mediaPicker.dismiss(animated: true)
+        track = mediaItemCollection.items[0]
+        trackName.text = mediaItemCollection.items[0].title
+        musicPlayer.setQueue(with: mediaItemCollection)
+        trackInfo.isHidden = false;
+    }
+    
+    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+        mediaPicker.dismiss(animated: true)
     }
     
     override func viewDidLoad() {
