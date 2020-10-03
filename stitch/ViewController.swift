9c52ec04aaef96d460e44fa784f50c218481596b
Sebastiaan Hols <s.hols@icloud.com>
2020-10-03 09:42:58 +0200

    Initial Setup + Media picker

--- stitch/ViewController.swift
+++ stitch/ViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MPMediaPickerControllerDelegate {
 
+    
+    @IBAction func pickTrack(_ sender: UIButton) {
+        let controller = MPMediaPickerController(mediaTypes: .music)
+        controller.allowsPickingMultipleItems = false
+        controller.popoverPresentationController?.sourceView = sender
+        controller.delegate = self
+        present(controller, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
